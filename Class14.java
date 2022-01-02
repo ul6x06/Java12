c8d963d2279fa0ff7770e672987f5766ce17a077
ul6x06 <91857915+ul6x06@users.noreply.github.com>
2022-01-03 03:45:40 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,10 @@
+
+package Java12;
+
+public class Class14 {
+	public static void main(String args[]) {
+		String str = "Habit is second nature.";
+		System.out.println(str);
+		System.out.println(str.substring(16, 22));
+	}
+}
